03e5dc8ed5f6fae54013253011f54f753105d8aa
Shekhar Gupta <shekhar@tricog.com>
2017-06-12 16:59:31 +0530

    Drop played and event handled

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -15,6 +15,8 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import java.util.Iterator;
+
 public class MyGdxGame extends ApplicationAdapter {
 
   //Render camera related
@@ -82,6 +84,8 @@ public class MyGdxGame extends ApplicationAdapter {
     batch.setProjectionMatrix(camera.combined);
     batch.begin();
     batch.draw(bucketImage, bucket.x, bucket.y);
+    for (Rectangle raindrop : raindrops)
+      batch.draw(dropImage, raindrop.x, raindrop.y);
     batch.end();
 
 
@@ -101,15 +105,29 @@ public class MyGdxGame extends ApplicationAdapter {
       bucket.x += 200 * Gdx.graphics.getDeltaTime();
 
     //restricting bucket to go beyond gameplay area
-    if (bucket.x < 0)
-      bucket.x = 0;
-    if (bucket.x > 800 - 64)
-      bucket.x = 800 - 64;
+    if (bucket.x < 0) bucket.x = 0;
+    if (bucket.x > 800 - 64) bucket.x = 800 - 64;
 
 
+    if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+      spawnRainDrop();
+
+    Iterator<Rectangle> iterator = raindrops.iterator();
+    while (iterator.hasNext()) {
+      Rectangle raindrop = iterator.next();
+      raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+
+      if(raindrop.overlaps(bucket)){
+        dropSound.play();
+        iterator.remove();
+      }
+
+      if (raindrop.y + 64 < 0) iterator.remove();
+    }
   }
 
 
+  //Method to add random drop
   private void spawnRainDrop() {
     Rectangle raindrop = new Rectangle();
     raindrop.x = MathUtils.random(0, 800 - 64);
@@ -118,5 +136,15 @@ public class MyGdxGame extends ApplicationAdapter {
     raindrop.height = 64;
     raindrops.add(raindrop);
     lastDropTime = TimeUtils.nanoTime();
+  }
+
+
+  @Override
+  public void dispose() {
+    dropImage.dispose();
+    bucketImage.dispose();
+    dropSound.dispose();
+    rainMusic.dispose();
+    batch.dispose();
   }
 }
